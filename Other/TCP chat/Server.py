896ff55c3099ec1d819d13fe5d1f20f6eee6b0d9
edvintopa <55089202+edvintopa@users.noreply.github.com>
2020-05-27 15:06:49 +0200

    TCP chat done

--- Other/TCP chat/Server.py
+++ Other/TCP chat/Server.py
@@ -1,11 +1,61 @@
-import socket
-import select
-import sys
-from thread import *
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
 
-IP_address = str(sys.argv[1])
-Port = int(sys.argv[2])
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server.bind(IP_address, Port)
+def accept_incoming_connections():
+    #Sets up handling for incoming clients.
+    while True:
+        client, client_address = SERVER.accept()
+        print("%s:%s has connected." % client_address)
+        client.send(bytes("Greetings from the cave! Now type your name and press enter!", "utf8"))
+        addresses[client] = client_address
+        Thread(target=handle_client, args=(client,)).start()
+
+
+def handle_client(client):  # Takes client socket as argument.
+    #Takes care of individual clients
+
+    name = client.recv(BUFSIZ).decode("utf8")
+    welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit.' % name
+    client.send(bytes(welcome, "utf8"))
+    msg = "%s has joined the chat!" % name
+    broadcast(bytes(msg, "utf8"))
+    clients[client] = name
+
+    while True:
+        msg = client.recv(BUFSIZ)
+        if msg != bytes("{quit}", "utf8"):
+            broadcast(msg, name+": ")
+        else:
+            client.send(bytes("{quit}", "utf8"))
+            client.close()
+            del clients[client]
+            broadcast(bytes("%s has left the chat." % name, "utf8"))
+            break
+
+
+def broadcast(msg, prefix=""):  # prefix is for name identification.
+    
+
+    for sock in clients:
+        sock.send(bytes(prefix, "utf8")+msg)
+
+        
+clients = {}
+addresses = {}
+
+HOST = ''
+PORT = 33000
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+SERVER = socket(AF_INET, SOCK_STREAM)
+SERVER.bind(ADDR)
+
+if __name__ == "__main__":
+    SERVER.listen(5)
+    print("Waiting for connection...")
+    ACCEPT_THREAD = Thread(target=accept_incoming_connections)
+    ACCEPT_THREAD.start()
+    ACCEPT_THREAD.join()
+    SERVER.close()
